c03e2291ca3db52b02ee9be9c055d1886521a686
dpatel <pateldiparth309@gmail.com>
2018-08-20 19:58:12 -0400

    body, spotlight added

--- src/app/module.ts
+++ src/app/module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BodyComponent } from './body/body';
 import { BaseComponent } from './base/base';
 import { HeaderComponent } from './header/header';
 import { SidebarComponent } from './sidebar/sidebar';
+import { SpotlightComponent } from './spotlight/spotlight';
 
 @NgModule({
   declarations: [
     BaseComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    BodyComponent,
+    SpotlightComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/spotlight/spotlight.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dp-spotlight',
+  templateUrl: './spotlight.html',
+  styleUrls: [ './spotlight.scss' ]
+})
+
+export class SpotlightComponent { }
