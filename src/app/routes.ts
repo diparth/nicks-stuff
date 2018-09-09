955582a6215400a5ec6e8463e9a8d550a77e51b0
dpatel <pateldiparth309@gmail.com>
2018-09-09 02:34:19 -0400

    style files added and comp tweak

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,5 +1,9 @@
 import { Ng2StateDeclaration } from '@uirouter/angular';
 import { AboutComponent } from './pages/about/about';
+import {EducationComponent} from './pages/education/education';
+import {ExperienceComponent} from './pages/experience/experience';
+import {ProjectsComponent} from './pages/projects/projects';
+import {ContactsComponent} from './pages/contacts/contacts';
 
 export const DP_ROUTES: Ng2StateDeclaration[] = [
   {
@@ -14,18 +18,22 @@ export const DP_ROUTES: Ng2StateDeclaration[] = [
   },
   {
     name: 'education',
-    url: '/education'
+    url: '/education',
+    component: EducationComponent
   },
   {
     name: 'experience',
-    url: '/experience'
+    url: '/experience',
+    component: ExperienceComponent
   },
   {
     name: 'projects',
-    url: '/projects'
+    url: '/projects',
+    component: ProjectsComponent
   },
   {
     name: 'contact',
-    url: '/contact'
+    url: '/contact',
+    component: ContactsComponent
   }
 ];
