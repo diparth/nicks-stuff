6ad5376bd13dc33faa53ba185919aae3f54f868d
diparthpatel <pateldiparth309@gmail.com>
2021-04-26 16:30:03 +0530

    welcome block added... animation added

--- src/app/base/base.ts
+++ src/app/base/base.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+declare var anime: any;
 
 @Component({
   selector: 'dp-root',
@@ -6,4 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./base.scss']
 })
 
-export class BaseComponent { }
+export class BaseComponent implements AfterViewInit {
+
+  public ngAfterViewInit(): void {
+    anime.timeline({ loop: false })
+      .add({
+        targets: '.wb .circle-container',
+        scale: [0, 1],
+        duration: 1500,
+        easing: "easeInOutExpo",
+        offset: '-=1000'
+      }).add({
+        targets: '.wb .circle-dark',
+        scale: [0, 1],
+        duration: 1000,
+        easing: "easeOutExpo",
+        offset: '-=600'
+      }).add({
+        targets: '.wb .letters-left',
+        scale: [0, 1],
+        duration: 1000,
+        offset: '-=550'
+      }).add({
+        targets: '.wb .bang',
+        scale: [0, 1],
+        duration: 1000,
+        offset: '-=1000'
+      });
+  }
+}
