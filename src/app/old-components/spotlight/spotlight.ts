dc692550f611678e368a8888db4c62e19a018f07
diparthpatel <pateldiparth309@gmail.com>
2022-09-12 19:44:43 +0530

    old clean up

--- src/app/old-components/spotlight/spotlight.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'dp-spotlight',
-  templateUrl: './spotlight.html',
-  styleUrls: [ './spotlight.scss' ]
-})
-
-export class SpotlightComponent { }
