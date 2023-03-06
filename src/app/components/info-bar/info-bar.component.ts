c50caea481487923947f80aa77c08238c14d715b
diparthpatel <pateldiparth309@gmail.com>
2023-03-06 17:16:50 +0530

    vcf downloader

--- src/app/components/info-bar/info-bar.component.ts
+++ src/app/components/info-bar/info-bar.component.ts
@@ -9,16 +9,32 @@ export class InfoBarComponent implements OnInit {
 
   constructor() { }
 
-  public vcfData: string = `
-    BEGIN:VCARD
-    VERSION:3.0
-    N:Diparth Patel
-    EMAIL;type=INTERNET;type=WORK;type=pref:pateldiparth309@gmail.com
-    TEL;type=IPHONE;type=CELL;type=VOICE;type=pref:+1 (226) 961-6511
-    END:VCARD
-  `;
+  public vcfData: string;
 
   public ngOnInit(): void {
   }
 
+  public downloadVCard(): void {
+    this.vcardData();
+
+    const a = document.createElement('a');
+    const file = new Blob([this.vcfData], { type: 'text/vcard' });
+
+    a.href = URL.createObjectURL(file);
+    a.download = 'diparth_patel.vcf';
+    a.click();
+
+    URL.revokeObjectURL(a.href);
+  }
+
+  private vcardData(): void {
+    this.vcfData = `BEGIN:VCARD
+    ${'VERSION:3.0'}
+    ${'N:'}
+    ${'FN:Diparth Patel'}
+    ${'TEL;TYPE=MOBILE,VOICE:+12269616511'}
+    ${'EMAIL;TYPE=email,PREF:pateldiparth309@gmail.com'}
+    REV:${new Date().toISOString()}
+    END:VCARD`.replace(/^\s\s\s\s/gm, ``);
+  }
 }
