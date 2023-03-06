8cf7657bddb6deb4c0eb033c2bc1cf4312d5a11f
diparthpatel <pateldiparth309@gmail.com>
2023-03-06 10:39:36 +0530

    components structure update + design tweaks

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ComingSoonComponent } from './components/coming-soon/coming-soon';
-
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'coming-soon'
+    redirectTo: 'home'
   },
   {
-    path: 'coming-soon',
-    component: ComingSoonComponent
+    path: 'home'
   }
 ];
 
